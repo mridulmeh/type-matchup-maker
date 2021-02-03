90247f1764f633372b920495e403548822042593
Mridul Meharia <mridul.meharia@forto.com>
2021-02-03 16:15:31 +0100

    Successfully added all test cases

--- src/utils/scoreKeepter.spec.ts
+++ src/utils/scoreKeepter.spec.ts
@@ -5,47 +5,61 @@ import { scoreKeeper } from "./scoreKeepter";
 describe("matchMaker", () => {
   let scoreBoardHandler;
   const matchupHandler = matchupMaker(["Rock", "Paper", "Scissors"]);
-  matchupHandler.update({
-    Rock: {
-      Attack: {
-        Scissors: 1,
-      },
-    },
-    Paper: {
-      Attack: {
-        Rock: 1,
-      },
-    },
-    Scissors: {
-      Attack: {
-        Paper: 1,
-      },
-    },
-  });
 
   it("should create a new scoreboard for a Matchup", () => {
     scoreBoardHandler = scoreKeeper(matchupHandler.get());
-    expect(scoreBoardHandler.get()).toEqual();
+    expect(scoreBoardHandler.getMatchup()).toEqual({
+      Rock: {},
+      Paper: {},
+      Scissors: {},
+    });
   });
 
-  it("should update scoreboard for a player", () => {
-    scoreBoardHandler = scoreBoardHandler.update(0, 2);
+  it("should set the Matchup", () => {
+    const matchup = {
+      Rock: {
+        Attack: {
+          Scissors: 1,
+        },
+        Defense: {
+          Paper: 1,
+        },
+      },
+      Paper: {
+        Attack: {
+          Rock: 3,
+        },
+      },
+      Scissors: {
+        Attack: {
+          Paper: 1,
+        },
+      },
+    };
+    scoreBoardHandler.setMatchup(matchup);
+    expect(scoreBoardHandler.getMatchup()).toEqual(matchup);
   });
 
-  it("should play first player's attacking turn", () => {
-    scoreBoardHandler = scoreBoardHandler.attack(0, "Rock");
-    scoreBoardHandler = scoreBoardHandler.defend(1, "Paper");
+  it("should update score for a player", () => {
+    scoreBoardHandler.setScore([0, 1]);
+    expect(scoreBoardHandler.getScore()).toEqual([0, 1]);
   });
-  it("should play second player's attacking turn", () => {
-    scoreBoardHandler = scoreBoardHandler.attack(0, "Paper");
-    scoreBoardHandler = scoreBoardHandler.defend(1, "Rock");
+
+  it("should play first turn", () => {
+    scoreBoardHandler.playTurn("Rock", "Paper");
+    expect(scoreBoardHandler.getScore()).toEqual([0, 3]);
+  });
+
+  it("should play second turn", () => {
+    scoreBoardHandler.playTurn("Paper", "Rock");
+    expect(scoreBoardHandler.getScore()).toEqual([2, 3]);
   });
 
   it("should set maximum number of turns", () => {
-    scoreBoardHandler = scoreBoardHandler.setMaxTurns(4);
+    scoreBoardHandler.setMaxTurns(4);
   });
 
   it("should set maximum score possible", () => {
-    scoreBoardHandler = setMaxScore(5);
+    scoreBoardHandler.setMaxScore(5);
   });
 });
--- src/utils/scoreKeepter.ts
+++ src/utils/scoreKeepter.ts
@@ -1,5 +1,56 @@
 import { Matchup } from "./matchupMaker";
 
-export const scoreKeeper = (matchup: Matchup) => {
-    
-}
+/**
+ * The objective of score is to have a turn with attack and defense:
+ *
+ * @param {Matchup} matchup
+ */
+export const scoreKeeper = (matchupInput: Matchup) => {
+  let matchup: Matchup = matchupInput;
+  let maxTurns = Number.POSITIVE_INFINITY;
+  let maxScore = Number.POSITIVE_INFINITY;
+  let score = [0, 0]; // 2 players constant for now
+
+  const setMaxTurns = (turns) => {
+    maxTurns = turns;
+  };
+
+  const setMaxScore = (newMaxScore) => {
+    maxScore = newMaxScore;
+  };
+
+  const setScore = (newScore) => {
+    score = newScore;
+  };
+
+  const setMatchup = (newMatchup: Matchup) => {
+    matchup = newMatchup;
+  };
+
+  const attack = (choiceA, choiceB) => {
+    const attackStat = matchup[choiceA].Attack?.[choiceB] ?? 0;
+    console.log(attackStat)
+    const defenseStat = matchup[choiceB].Defense?.[choiceA] ?? 0;
+
+    return Math.max(0, attackStat - defenseStat);
+  };
+
+  const playTurn = (playerAChoice, playerBChoice) => {
+    const playerAScoreChange = attack(playerAChoice, playerBChoice);
+    console.log(playerAScoreChange)
+    const playerBScoreChange = attack(playerBChoice, playerAChoice);
+    setScore([score[0] + playerAScoreChange, score[1] + playerBScoreChange]);
+  };
+
+  return {
+    getMatchup: () => matchup,
+    getScore: () => score,
+    getMaxScore: () => maxScore,
+    getMaxTurns: () => maxTurns,
+    playTurn,
+    setScore,
+    setMatchup,
+    setMaxScore,
+    setMaxTurns,
+  };
+};
