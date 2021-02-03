8e37f3c2de1fb57ff5eda9cdd3d6a0db42550b61
Mridul Meharia <mridul.meharia@forto.com>
2021-02-03 16:48:00 +0100

    Removed needless imports

--- src/components/rock-paper-scissors/rock-paper-scissors.tsx
+++ src/components/rock-paper-scissors/rock-paper-scissors.tsx
@@ -1,10 +1,6 @@
 import React from "react";
 import { ScoreBoardHandler } from "../../utils/scoreKeeper";
-import { GameArea } from "../game-area";
 import { GameBoard } from "../game-board";
-import { Header } from "../header";
-import { ScoreBoard } from "../score-board/score-board";
-import { Settings } from "../settings";
 import { createRockPaperScissorsGame } from "./gameCreator";
 
 export const RockPaperScissors: React.FC = () => {
